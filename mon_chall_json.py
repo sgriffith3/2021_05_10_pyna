f70afd7717ea6d082be504f5fce8326d4044fa29
Sam <sgriffith@alta3.com>
2021-05-10 19:25:34 +0000

    created first challenge

---
+++ mon_chall_json.py
@@ -0,0 +1,38 @@
+# Put the following JSON data into a file called status.json
+#
+#    [
+#      {
+#        "state": "up",
+#        "server": "001"
+#      },
+#      {
+#        "state": "down",
+#        "server": "002"
+#      },
+#      {
+#        "state": "up",
+#        "server": "003"
+#      },
+#      {
+#        "state": "up",
+#        "server": "004"
+#      },
+#      {
+#        "state": "down",
+#        "server": "005"
+#      },
+#      {
+#        "state": "up",
+#        "server": "006"
+#      },
+#      {
+#        "state": "down",
+#        "server": "007"
+#      }
+#    ]
+#
+
+#It is your job to take JSON data from status.json, sort out the server names that are "down", and put their names into a new file called downed_servers.txt
+
+#ROCKET SCIENCE CHALLENGE: return the full json for only the downed servers into a file called downed_servers.json
+
